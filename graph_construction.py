702428c27514dda77463efa6886e62160095387c
LJividen <150537915+LJividen@users.noreply.github.com>
2024-08-23 09:12:48 -0400

    Add files via upload

--- graph_construction.py
+++ graph_construction.py
@@ -1,224 +1,162 @@
 import pandas as pd
-import warnings
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import torch
-import matplotlib.pyplot as plt
+from torch_geometric.data import Data
+from torch_geometric.utils import to_undirected
+import sklearn.model_selection
 
-warnings.filterwarnings("ignore")
+# mordred descriptor data path
+DESCRIPTOR_PATH = '/data/bai/Env_Health/cleanData/sortedFeat_impDesc.csv'
+# molecular fingerprint path
+FINGERPRINT_PATH = '/data/bai/Env_Health/cleanData/AP2D_count_stand.csv'
 
-# load in labeled dataset and OECD unlabeled dataset
-df_mordred = pd.read_csv('/data/bai/Env_Health/cleanData/sortedFeat_impDesc.csv')
+# Assign desciptor and fingerprint to features or graph construction (similarity scores)
+# best model is using descriptor for features and descriptor for similarity 
+SIMILARITY_SCORE = 'fingerprint'
 
-df_fp = pd.read_csv('/data/bai/Env_Health/cleanData/AP2D_count_stand.csv')
-# Filter rows for the portal data to drop them
-portal_rows = df_fp[df_fp['Region'] == 'nonportal']
-# Get the indexes of the filtered rows
-portal_indexes = portal_rows.index
+# define minimum # of edges per PFAS
+# defines the number of top N unique similarity scores to construct edges with
+EDGES_PER_PFAS = 4
 
-df_fp = df_fp.drop(columns= ['SMILES','docking_score','Region'])
-df_label = pd.concat([df_mordred, df_fp], axis=1)
-
-df_label_dropcols = df_label.drop(columns=['Region', 'MAXssNH2']) # dropped extra feature as the OECD data provided no values for it
-df_label_dropcols = df_label_dropcols.drop(index=portal_indexes)
-
-df_OECD = pd.read_csv('/data/bai/Env_Health/cleanData/AP2D_count_OECD.csv')
-df_OECD['docking_score'] = 0
-
-## drop the following rows from the df [2101, 2102]  as they are not PFAS (OP+O)
-zero_chain_indices = [2101, 2102] 
-df_OECD = df_OECD.drop(index=zero_chain_indices)
+# Constructed graph save path
+GRAPH_PATH = ''
 
 
-# Ensure indices are unique by resetting them
-df_label_dropcols.reset_index(drop=True, inplace=True)
-df_OECD.reset_index(drop=True, inplace=True)
+def load_data():
+    df_mordred = pd.read_csv(DESCRIPTOR_PATH)
+    df_fp = pd.read_csv(FINGERPRINT_PATH)
+    
+    # Filter and drop portal data
+    portal_rows = df_fp[df_fp['Region'] == 'nonportal']
+    portal_indexes = portal_rows.index
+    df_fp = df_fp.drop(columns=['SMILES', 'docking_score', 'Region'])
+    df_label = pd.concat([df_mordred, df_fp], axis=1)
+    
+    # Drop region column and a descriptor with missing values
+    df_label_dropcols = df_label.drop(columns=['Region', 'MAXssNH2']).drop(index=portal_indexes)
+    df_OECD = pd.read_csv('/data/bai/Env_Health/cleanData/AP2D_count_OECD.csv')
+    df_OECD = df_OECD.drop(index=[2101, 2102])  # Drop non-PFAS rows
+    
+    # Reset indices and concatenate datasets
+    df_label_dropcols.reset_index(drop=True, inplace=True)
+    df_OECD.reset_index(drop=True, inplace=True)
+    df_combined = pd.concat([df_label_dropcols, df_OECD], ignore_index=True)
+    df_drop_dockingScore_SMILES = df_combined.drop(columns=['docking_score', 'SMILES'])
+    
+    return df_drop_dockingScore_SMILES, df_combined
 
-# Concatenate the DataFrames
-df_combined = pd.concat([df_label_dropcols, df_OECD], ignore_index=True)
+def assign_sim_and_feat(df, similarity=SIMILARITY_SCORE, fingerprint_name='AP2D_count'):
+    # Define column prefixes based on fingerprint name
+    col_prefix_map = {
+        'AP2D': 'AD2D', 'AP2D_count': 'APC2D', 'cdk': 'FP', 'cdk_graphonly': 'GraphFP',
+        'cdke': 'ExtFP', 'kr': 'KRFP', 'kr_count': 'KRFPC', 'maccs': 'MACCSFP',
+        'pubchem': 'PubchemFP', 'substructure': 'SubFP', 'substructure_count': 'SubFPC'
+    }
+    col_abbrev = col_prefix_map.get(fingerprint_name, 'FP')
 
-df_combined_drop = df_combined.drop(columns= ['docking_score','SMILES'])
-
-### create function to split the data for similarity calculation and prediction
-def assign_sim_and_feat(df, similarity= 'fingerprint', fingerprint_name= 'AP2D_count'):
-    # check what features are to be used for similarity score
-    if fingerprint_name == 'AP2D':
-        col_abbrev = 'AD2D'
-
-    elif fingerprint_name == 'AP2D_count':
-        col_abbrev = 'APC2D'
-
-    elif fingerprint_name == 'cdk':
-        col_abbrev = 'FP'
-
-    elif fingerprint_name == 'cdk_graphonly':
-        col_abbrev = 'GraphFP'
-
-    elif fingerprint_name == 'cdke':
-        col_abbrev = 'ExtFP'
-
-    elif fingerprint_name == 'kr':
-        col_abbrev = 'KRFP'
-
-    elif fingerprint_name == 'kr_count':
-        col_abbrev = 'KRFPC'
-
-    elif fingerprint_name == 'maccs':
-        col_abbrev = 'MACCSFP'
-
-    elif fingerprint_name == 'pubchem':
-        col_abbrev = 'PubchemFP'
-
-    elif fingerprint_name == 'substructure':
-        col_abbrev = 'SubFP'
-
-    elif fingerprint_name == 'substructure_count':
-        col_abbrev = 'SubFPC'
-
+    # Select columns based on similarity type
     if similarity == 'fingerprint':
         filtered_columns = [col for col in df.columns if col.startswith(col_abbrev)]
         similarity_data = df[filtered_columns]
-        feature_data = df.drop(columns= filtered_columns)
-    
-    if similarity == 'descriptor':
+        feature_data = df.drop(columns=filtered_columns)
+    elif similarity == 'descriptor':
         filtered_columns = [col for col in df.columns if not col.startswith(col_abbrev)]
         similarity_data = df[filtered_columns]
-        feature_data = df.drop(columns= filtered_columns)
-
+        feature_data = df.drop(columns=filtered_columns)
+    
     return similarity_data, feature_data
 
-similarity_data, feature_data = assign_sim_and_feat(df_combined_drop, similarity= 'fingerprint', fingerprint_name= 'AP2D_count')
-
-def get_cosine_similarity(df_with_similarity_feat):
-    from sklearn.metrics.pairwise import cosine_similarity
-    
-    # Convert DataFrame to numpy array
+def calculate_cosine_similarity(df_with_similarity_feat):
+    # creates dataframe of cosine scores for every possible combination
     data_array = df_with_similarity_feat.to_numpy(dtype=float)
-
-    # Calculate the cosine similarity matrix
     similarity_matrix = cosine_similarity(data_array)
-
-    # Function to get upper triangle indices excluding the diagonal
-    def get_upper_triangle_indices(n):
-        rows, cols = np.triu_indices(n, k=1)
-        return rows, cols
-
-    # Get the upper triangle indices (excluding the diagonal)
-    rows, cols = get_upper_triangle_indices(data_array.shape[0])
-
-    # Extract the similarity scores for the upper triangle
+    rows, cols = np.triu_indices(data_array.shape[0], k=1)
     similarity_scores = similarity_matrix[rows, cols]
-
-    # Combine the row and column indices with similarity scores
-    cos_sim_results = list(zip(rows, cols, similarity_scores))
-
-    # Convert the list to a DataFrame
-    df_cos_scores = pd.DataFrame(cos_sim_results, columns=['Query Index', 'Target Index', 'Cosine Similarity'])
-
+    df_cos_scores = pd.DataFrame(list(zip(rows, cols, similarity_scores)),
+                                 columns=['Query Index', 'Target Index', 'Cosine Similarity'])
     return df_cos_scores
 
-df_cos_scores = get_cosine_similarity(similarity_data)
-
 def N_closest_nodes_edgeConstruction(df_similarity_score, edges_sent_from_each_PFAS):
-    from torch_geometric.data import Data
-    from torch_geometric.utils import to_undirected
     df = df_similarity_score.copy()
-    # Ensure 'Query Index' and 'Target Index' are of integer type
     df['Query Index'] = df['Query Index'].astype(int)
     df['Target Index'] = df['Target Index'].astype(int)
-
-    # Create a DataFrame to store the top n similarities for each compound
+    
     top_similarities = pd.DataFrame()
-
-    # Find the top N similarities for each compound, excluding self-similarities
     all_compounds = pd.concat([df['Query Index'], df['Target Index']]).unique()
     for compound in all_compounds:
         top_sim = df[
-            ((df['Query Index'] == compound) | (df['Target Index'] == compound)) & (df['Query Index'] != df['Target Index'])
+            ((df['Query Index'] == compound) | (df['Target Index'] == compound)) & 
+            (df['Query Index'] != df['Target Index'])
         ].nlargest(edges_sent_from_each_PFAS, 'Cosine Similarity')
-
         top_similarities = pd.concat([top_similarities, top_sim])
 
-    # using top similarites 
-    # Extract the edges
     edges = top_similarities[['Query Index', 'Target Index']].values.T
-
-    # Create the graph using PyTorch Geometric
     edge_index = torch.tensor(edges, dtype=torch.long)
     data = Data(edge_index=edge_index)
-
-    # Convert to undirected graph
     data.edge_index = to_undirected(data.edge_index)
-
-    # check if data successfully converted to undirected
-    if data.is_directed() == True:
-        return ('Graph is not undirected after conversion attempt')
+    
+    if data.is_directed():
+        return 'Graph is not undirected after conversion attempt'
     
     return data
 
+def split_data(df_combined, feature_data, target='docking_score'):
+    y = np.array(df_combined[target])
+    x = np.array(feature_data)
+    
+    unlabeled_indexes = np.where(y == 0)[0]
+    excluded_mask = np.zeros(len(y), dtype=bool)
+    excluded_mask[unlabeled_indexes] = True
+    
+    x_temp, x_test, y_temp, y_test, temp_indices, test_indices = sklearn.model_selection.train_test_split(
+        x[~excluded_mask], y[~excluded_mask], np.arange(len(y))[~excluded_mask], test_size=0.2, random_state=420
+    )
+    
+    x_train, x_val, y_train, y_val, train_indices, val_indices = sklearn.model_selection.train_test_split(
+        x_temp, y_temp, temp_indices, test_size=0.2, random_state=420
+    )
+    
+    x_test = np.concatenate((x_test, x[excluded_mask]), axis=0)
+    y_test = np.concatenate((y_test, y[excluded_mask]), axis=0)
+    test_indices = np.concatenate((test_indices, np.arange(len(y))[excluded_mask]), axis=0)
+    
+    return x_train, x_val, x_test, y_train, y_val, y_test, train_indices, val_indices, test_indices
 
-edges_per_pfas = 4
-data = N_closest_nodes_edgeConstruction(df_cos_scores, edges_per_pfas)
+def create_masks(df_combined, train_indices, val_indices, test_indices):
+    num_nodes = len(df_combined['docking_score'].values)
+    
+    train_mask = torch.zeros(num_nodes, dtype=torch.bool)
+    val_mask = torch.zeros(num_nodes, dtype=torch.bool)
+    test_mask = torch.zeros(num_nodes, dtype=torch.bool)
+    
+    train_mask[train_indices] = True
+    val_mask[val_indices] = True
+    test_mask[test_indices] = True
+    
+    return train_mask, val_mask, test_mask
 
-### Create training, test, and val sets ONLY of labeled data
-# create an array of the actual value y and an array from the feature_data
-target = 'docking_score'
-y = np.array(df_combined[target])
-## predict on descriptor as well for this one
-x = np.array(feature_data)
+def assign_node_attributes(data, feature_data, df_combined, train_mask, val_mask, test_mask):
+    node_features = feature_data.values
+    node_labels = df_combined['docking_score'].values
+    
+    data.x = torch.tensor(node_features, dtype=torch.float32)
+    data.y = torch.tensor(node_labels, dtype=torch.float32)
+    data.train_mask = train_mask
+    data.val_mask = val_mask
+    data.test_mask = test_mask
+    
+    return data
 
-# get the indexes of the unlabeled data
-unlabeled_indexes = np.where(y == 0)[0]
-
-# Create masks for excluded rows
-excluded_mask = np.zeros(len(y), dtype=bool)
-excluded_mask[unlabeled_indexes] = True
-
-import sklearn.model_selection
-# Create initial train-test split excluding the specified indexes
-x_temp, x_test, y_temp, y_test, temp_indices, test_indices = sklearn.model_selection.train_test_split(
-    x[~excluded_mask], y[~excluded_mask], np.arange(len(y))[~excluded_mask], test_size=0.2, random_state=420
-)
-
-# Split the remaining data into training and validation sets
-x_train, x_val, y_train, y_val, train_indices, val_indices = sklearn.model_selection.train_test_split(
-    x_temp, y_temp, temp_indices, test_size=0.2, random_state=420
-)
-
-# Add the excluded rows to the test set
-x_test = np.concatenate((x_test, x[excluded_mask]), axis=0)
-y_test = np.concatenate((y_test, y[excluded_mask]), axis=0)
-
-test_indices = np.concatenate((test_indices, np.arange(len(y))[excluded_mask]), axis=0)
-
-# Number of nodes
-num_nodes = len(df_combined['docking_score'].values)
-
-# Create masks
-train_mask = torch.zeros(num_nodes, dtype=torch.bool)
-val_mask = torch.zeros(num_nodes, dtype=torch.bool)
-test_mask = torch.zeros(num_nodes, dtype=torch.bool)
-
-# Set masks to True for corresponding indices
-train_mask[train_indices] = True
-val_mask[val_indices] = True
-test_mask[test_indices] = True
-
-## Assign attributes to the nodes
-# Convert DataFrame to a NumPy array
-node_features = feature_data.values
-node_labels = df_combined['docking_score'].values
-
-# Convert to PyTorch tensor
-x = torch.tensor(node_features, dtype=torch.float32)
-y = torch.tensor(node_labels, dtype=torch.float32)
-
-# Assign node attributes to the graph
-data.train_mask = train_mask
-data.val_mask = val_mask
-data.test_mask = test_mask
-data.y = y
-data.x = x
-
-# Save the graph and data
-torch.save(data, '4_edge_cosSim_AP2DC_nonportal_discon.pt')
+if __name__ == "__main__":
+    df_drop_dockingScore_SMILES, df_combined = load_data()
+    similarity_data, feature_data = assign_sim_and_feat(df_drop_dockingScore_SMILES)
+    df_cos_scores = calculate_cosine_similarity(similarity_data)
+    
+    data = N_closest_nodes_edgeConstruction(df_cos_scores, EDGES_PER_PFAS)
+    
+    x_train, x_val, x_test, y_train, y_val, y_test, train_indices, val_indices, test_indices = split_data(df_combined, feature_data)
+    train_mask, val_mask, test_mask = create_masks(df_combined, train_indices, val_indices, test_indices)
+    
+    data = assign_node_attributes(data, feature_data, df_combined, train_mask, val_mask, test_mask)
+    torch.save(data, GRAPH_PATH)
--- model.py
+++ model.py
@@ -3,33 +3,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import SAGEConv
 from torch_geometric.data import DataLoader
-import torch.nn.init as init
-import matplotlib.pyplot as plt
 import os
-import itertools
 
-# Load the graph and data
-data = torch.load('4_edge_cosSim_AP2D_count_nonportal_discon.pt')
+# Created graph for GCN Path
+GRAPH_PATH = "./4_edge_cosSim_AP2D_count_nonportal_discon.pt"
+# Model save path
+MODEL_PATH = ""
 
-# Check if a GPU is available
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using {device} device")
+# Best model parameters
+final_params = {
+    'hidden_layers': [17],
+    'activation_function': F.leaky_relu,
+    'dropout': True,
+    'dropout_rate': 0.15,
+    'batch_norm': True,
+    'batch_size': 150,
+    'learning_rate': 35,
+    'epochs': 4000
+}
 
-def weights_init_normal(m):
-    if isinstance(m, SAGEConv):
-        init.normal_(m.lin_l.weight.data, mean=0.0, std=0.02)
-        if m.lin_l.bias is not None:
-            init.constant_(m.lin_l.bias.data, 0.0)
-        init.normal_(m.lin_r.weight.data, mean=0.0, std=0.02)
-        if m.lin_r.bias is not None:
-            init.constant_(m.lin_r.bias.data, 0.0)
-    elif isinstance(m, nn.Linear):
-        init.normal_(m.weight.data, mean=0.0, std=0.02)
-        if m.bias is not None:
-            init.constant_(m.bias.data, 0.0)
-
+# Define the model
 class SAGENet(nn.Module):
-    def __init__(self, in_feats, hidden_layers, activation=F.relu, dropout=False, dropout_rate=0.5, batch_norm=False):
+    def __init__(self, in_feats, hidden_layers, activation, dropout, dropout_rate, batch_norm):
         super(SAGENet, self).__init__()
         self.layers = nn.ModuleList()
         self.bns = nn.ModuleList() if batch_norm else None
@@ -66,18 +61,16 @@ class SAGENet(nn.Module):
         x = self.layers[-1](x, edge_index)
         return x
 
+# Train the model
 def train(data, model, device, epochs, batch_size, lr, patience=10):
     model.train()
-    optimizer = torch.optim.Adam(model.parameters(), lr= lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     best_val_loss = float('inf')
     best_model = None
     epochs_no_improve = 0
 
-    # Create DataLoader for batching
+    # DataLoader for batching
     loader = DataLoader([data], batch_size=batch_size, shuffle=True, num_workers=0)
-
-    train_losses = []
-    val_losses = []
 
     for epoch in range(epochs):
         for batch in loader:
@@ -87,16 +80,12 @@ def train(data, model, device, epochs, batch_size, lr, patience=10):
 
             # Compute loss
             loss = F.mse_loss(logits[batch.train_mask], batch.y[batch.train_mask].float())
-
             loss.backward()
             optimizer.step()
 
             # Evaluation
             with torch.no_grad():
                 val_loss = F.mse_loss(logits[batch.val_mask], batch.y[batch.val_mask].float())
-
-            train_losses.append(loss.item())
-            val_losses.append(val_loss.item())
 
             # Save the best validation loss
             if best_val_loss > val_loss:
@@ -107,34 +96,15 @@ def train(data, model, device, epochs, batch_size, lr, patience=10):
                 epochs_no_improve += 1
 
         if epoch % 5 == 0:
-            print(
-                f"In epoch {epoch}, train loss: {loss:.3f}, val loss: {val_loss:.3f} (best {best_val_loss:.3f})"
-            )
+            print(f"Epoch {epoch}: train loss = {loss:.3f}, val loss = {val_loss:.3f} (best = {best_val_loss:.3f})")
 
         if epochs_no_improve >= patience:
             print(f"Early stopping after {epoch} epochs with best validation loss: {best_val_loss:.3f}")
             break
 
-    actual_epochs = len(train_losses)  # Track the actual number of epochs run
-    return best_model, train_losses, val_losses, best_val_loss, actual_epochs
+    return best_model
 
-def create_train_plot(epochs, train_losses, val_losses, learning_rate, hidden_layers, batch_size, plot_path):
-    # Plot training and validation losses
-    plt.figure()
-    plt.plot(range(1, epochs + 1), train_losses, label='Training Loss')
-    plt.plot(range(1, epochs + 1), val_losses, label='Validation Loss')
-
-    # Add subtitle with parameters
-    subtitle_text = f'Parameters: LR={learning_rate}, HL={len(hidden_layers)}, HLS={hidden_layers}, BS={batch_size}\n'
-    plt.text(0.5, 1.05, subtitle_text, horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes)
-
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.title('Training and Validation Loss per Epoch')
-    plt.legend()
-    plt.savefig(plot_path)
-    plt.close()
-
+# Save model
 def save_model(model, file_path, params):
     state = {
         'model_state_dict': model.state_dict(),
@@ -142,75 +112,19 @@ def save_model(model, file_path, params):
     }
     torch.save(state, file_path)
 
-def get_activation_function_name(activation_function):
-    if activation_function == F.relu:
-        return 'relu'
-    elif activation_function == F.leaky_relu:
-        return 'leakyrelu'
-    else:
-        return 'unknown'
-    
+# Main script
+if __name__ == "__main__":
+    # Load data
+    data = torch.load(GRAPH_PATH)
 
-# Function to perform grid search for hyperparameters and train models
-def grid_search(data, device, param_grid):
+    # Check for GPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    keys, values = zip(*param_grid.items())
-    param_combinations = [dict(zip(keys, v)) for v in itertools.product(*values)]
+    # Initialize and train the model
+    model = SAGENet(data.num_features, final_params['hidden_layers'], final_params['activation_function'], 
+                    final_params['dropout'], final_params['dropout_rate'], final_params['batch_norm']).to(device)
+    model = train(data, model, device, final_params['epochs'], final_params['batch_size'], 
+                  final_params['learning_rate'], patience=150)
 
-    results = []
-
-    for params in param_combinations:
-        hidden_layers = params['hidden_layers']
-        activation_function = params.get('activation_function', F.relu)
-        dropout = params.get('dropout', False)
-        dropout_rate = params.get('dropout_rate', 0.5)
-        batch_norm = params.get('batch_norm', False)
-        batch_size = params.get('batch_size', 32)
-        learning_rate = params.get('learning_rate', 0.01)
-        epochs = params.get('epochs', 1000)
-
-        act_name = get_activation_function_name(activation_function)
-
-        param_set = {
-            'hidden_layers': hidden_layers,
-            'activation_function': activation_function,
-            'dropout': dropout,
-            'dropout_rate': dropout_rate, 
-            'batch_norm': batch_norm,
-            'batch_size': batch_size,
-            'learning_rate': learning_rate
-        }
-        
-        model = SAGENet(data.num_features, hidden_layers, activation_function, dropout, dropout_rate, batch_norm).to(device)
-        # # Apply the weight initialization
-        # model.apply(weights_init_normal)
-        model, train_losses, val_losses, best_val_loss, act_epochs = train(data, model, device, epochs, batch_size, learning_rate, patience=150)
-
-        # Save the model
-        save_dir = 'gridSearch_models'
-        model_path = os.path.join(save_dir,
-            f"model_hl{hidden_layers}_dr{dropout}_drR{dropout_rate}_bn{batch_norm}_bs{batch_size}_{act_name}_lr{learning_rate}.pt"
-        )
-        #torch.save(model.state_dict(), model_path)
-        save_model(model, model_path, param_set)
-
-        # Save the training plot
-        plot_path = os.path.join(save_dir,
-            f"plot_hl{hidden_layers}_dr{dropout}_drR{dropout_rate}_bn{batch_norm}_bs{batch_size}_{act_name}_lr{learning_rate}.png"
-        )
-        create_train_plot(act_epochs, train_losses, val_losses, learning_rate, hidden_layers, batch_size, plot_path)
-
-# Define hyperparameter grid
-param_grid = {
-    'hidden_layers': [[17], [16], [18]], #[1000], [500], [100], [1000,500], [1000,500,100], [32], [32,16], [32,16,32], [16,32,16]
-    'activation_function': [F.leaky_relu], #F.relu, 
-    'dropout': [True], # True, 
-    'dropout_rate': [.11,.13,.15, .17, .19], #, .4, 0.5, .6
-    'batch_norm': [True],
-    'batch_size': [130,135,140,145,150,155,160,165,170],
-    'learning_rate': [.31,33,35,.37,.39],
-    'epochs': [4000]
-}
-
-# Run grid search
-grid_search(data, device, param_grid)
+    # Save the final model
+    save_model(model, MODEL_PATH, final_params)
